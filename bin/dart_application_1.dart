d227f518914c9a8bd0d2ba7a69dab55c404d8592
Mazen <faisal@alMacBook-Pro-alkhas-bFaisal.local>
2022-05-18 13:57:39 +0300

    Hw1

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,21 @@
+import 'dart:collection';
+import 'dart:io';
+
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+
+void main(List<String> arguments) {
+  // this variable indcate mazen's number
+var phoneNumber ='0582779616';
+//this is the name that will print
+var name='mazen alotaibi';
+//it is mazen's birthday
+const dayOfBirth='2/11/2001';
+
+print('Hi may name is ${name}, my birthday is ${dayOfBirth}, my number is ${phoneNumber} ');
+print('the fourth index of my number is ${phoneNumber[4]}');
+print('my nanme contain ${name.length} letter');
+
+exit(0);
+}
+
+
